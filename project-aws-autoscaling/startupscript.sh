9d2a898d546446fc0674cf474ad59f5cac193b54
henrywangpuppet <53325547+henrywangpuppet@users.noreply.github.com>
2020-05-04 11:48:39 +0800

    Update startupscript.sh

--- project-aws-autoscaling/startupscript.sh
+++ project-aws-autoscaling/startupscript.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+#Setup Configuration Variable First
+
+momhostname=<MoM Hostname>
+rbactoken=<RBAC Token>
 
 #Make sure a Puppet run happens before informing MoM
 sudo -i puppet agent -t
 
 #trigger a MoM agent run to inform the compiler has been back
-/bin/curl -k -H 'X-Authentication:0CCOlIZsIOr1fb92ER6iYbZ9RZeeMEiWyyX4U-75gQVI' \
-https://ip-10-0-0-100.ap-southeast-1.compute.internal:8143/orchestrator/v1/command/deploy \
--X POST -d '{"environment":"production","scope": {"nodes":  ["ip-10-0-0-100.ap-southeast-1.compute.internal"]}}' \
+/bin/curl -k -H "X-Authentication:${rbactoken}" \
+https://${momhostname}:8143/orchestrator/v1/command/deploy \
+-X POST -d "{\"environment\":\"production\",\"scope\": {\"nodes\":  [\"${momhostname}\"]}}" \
 -H "Content-Type: application/json"
