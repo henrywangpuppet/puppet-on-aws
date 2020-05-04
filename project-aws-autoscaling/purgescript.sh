e0fa234e18f26141266a22a4f4c4c4cc8c829ea8
henrywangpuppet <53325547+henrywangpuppet@users.noreply.github.com>
2020-05-04 11:41:54 +0800

    Update purgescript.sh

--- project-aws-autoscaling/purgescript.sh
+++ project-aws-autoscaling/purgescript.sh
@@ -1,38 +1,46 @@
 #!/bin/bash
 #Purge Node Wheneven it is stopped
+#Setup Configuration Variables First
+
+momhostname=<MoM Hostname>
+rbactoken=<RBAC Token>
+
+capath=<ca.pem Path>
+cerfilepath=<cert.pem Path>
+keyfilepath=<key.pem Path>
 
 /bin/curl -X PUT \
 --tlsv1 \
---cacert /home/ec2-user/nodepurge/ca.pem \
---cert /home/ec2-user/nodepurge/aws-lambada.example.com.pem \
---key /home/ec2-user/nodepurge/aws-lambada.example.com.key.pem \
+--cacert ${capath} \
+--cert ${cerfilepath} \
+--key ${keyfilepath} \
 -H "Accept: application/json" \
 -H "Content-Type: application/json" \
 -d '{"desired_state":"revoked"}' \
-https://ip-10-0-0-100.ap-southeast-1.compute.internal:8140/puppet-ca/v1/certificate_status/$HOSTNAME
+https://${momhostname}:8140/puppet-ca/v1/certificate_status/$HOSTNAME
 
 /bin/curl -X DELETE \
 --tlsv1 \
---cacert /home/ec2-user/nodepurge/ca.pem \
---cert /home/ec2-user/nodepurge/aws-lambada.example.com.pem \
---key /home/ec2-user/nodepurge/aws-lambada.example.com.key.pem \
+--cacert ${capath} \
+--cert ${cerfilepath} \
+--key ${keyfilepath} \
 -H "Accept: application/json" \
-https://ip-10-0-0-100.ap-southeast-1.compute.internal:8140/puppet-ca/v1/certificate_status/$HOSTNAME
+https://${momhostname}:8140/puppet-ca/v1/certificate_status/$HOSTNAME
 
 /bin/curl -X POST \
 --tlsv1 \
---cacert /home/ec2-user/nodepurge/ca.pem \
---cert /home/ec2-user/nodepurge/aws-lambada.example.com.pem \
---key /home/ec2-user/nodepurge/aws-lambada.example.com.key.pem \
+--cacert ${capath} \
+--cert ${cerfilepath} \
+--key ${keyfilepath} \
 -H "Accept: application/json" \
 -H "Content-Type: application/json" \
 -d "{\"command\":\"deactivate node\",\"version\":3,\"payload\":{\"certname\":\"$HOSTNAME\"}}" \
-https://ip-10-0-0-100.ap-southeast-1.compute.internal:8081/pdb/cmd/v1
+https://${momhostname}:8081/pdb/cmd/v1
 
 #trigger a MoM agent run to inform the compiler has been purged
-/bin/curl -k -H 'X-Authentication:0CCOlIZsIOr1fb92ER6iYbZ9RZeeMEiWyyX4U-75gQVI' \
-https://ip-10-0-0-100.ap-southeast-1.compute.internal:8143/orchestrator/v1/command/deploy \
--X POST -d '{"environment":"production","scope": {"nodes":  ["ip-10-0-0-100.ap-southeast-1.compute.internal"]}}' \
+/bin/curl -k -H "X-Authentication:${rbactoken}" \
+https://${momhostname}:8143/orchestrator/v1/command/deploy \
+-X POST -d "{\"environment\":\"production\",\"scope\": {\"nodes\":  [\"${momhostname}\"]}}" \
 -H "Content-Type: application/json"
 
 #Remove local SSL keys so that agent is runable next time it is booted up.
