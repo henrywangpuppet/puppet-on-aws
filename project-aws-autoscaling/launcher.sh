4355be17999510ac31ebc21da85d313061b6e132
henrywangpuppet <53325547+henrywangpuppet@users.noreply.github.com>
2020-05-12 07:53:11 +0800

    Update launcher.sh

--- project-aws-autoscaling/launcher.sh
+++ project-aws-autoscaling/launcher.sh
@@ -8,15 +8,8 @@ rbactoken=<RBAC Token>
 #install Puppet Agent
 /bin/curl -k https://${momhostname}:8140/packages/current/install.bash | sudo bash -s main:dns_alt_names=$awslbdns extension_requests:pp_role=awsloadbalancer
 
-#Refresh MoM puppet agent run
-/bin/curl -k -H "X-Authentication:${rbactoken}" \
-https://${momhostname}:8143/orchestrator/v1/command/deploy \
--X POST -d "{\"environment\":\"production\",\"scope\": {\"nodes\":  [\"${momhostname}\"]}}" \
--H "Content-Type: application/json"
-
 #enable startup and shutdown script
 cp /home/awsnodemanagement/nodepurge.service /etc/systemd/system/nodepurge.service
 systemctl daemon-reload
 systemctl enable nodepurge.service
 systemctl start nodepurge.service
-
