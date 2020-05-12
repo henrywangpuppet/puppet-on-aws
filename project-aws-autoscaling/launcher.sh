87e8ec0a5db6e0b0c426f0da67481cf331384bbe
henrywangpuppet <53325547+henrywangpuppet@users.noreply.github.com>
2020-05-12 08:40:21 +0800

    Update launcher.sh

--- project-aws-autoscaling/launcher.sh
+++ project-aws-autoscaling/launcher.sh
@@ -6,7 +6,7 @@ awslbdns=<AWS LoadBalancer DNS Name>
 rbactoken=<RBAC Token>
 
 #install Puppet Agent
-/bin/curl -k https://${momhostname}:8140/packages/current/install.bash | sudo bash -s main:dns_alt_names=$awslbdns extension_requests:pp_role=awsloadbalancer
+/bin/curl -k https://${momhostname}:8140/packages/current/install.bash | sudo bash -s main:dns_alt_names=$awslbdns extension_requests:pp_role=awsloadbalancer -- --puppet-service-ensure stopped
 
 #enable startup and shutdown script
 cp /home/awsnodemanagement/nodepurge.service /etc/systemd/system/nodepurge.service
